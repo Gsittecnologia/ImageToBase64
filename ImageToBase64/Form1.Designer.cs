1b32cb7b39c04e77044880781a190a4deaf0f475
Alexandre Santos <alexandre.programming@outlook.com>
2018-08-23 13:28:37 -0300

    Add project files.

---
+++ ImageToBase64/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace ImageToBase64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.svd = new System.Windows.Forms.SaveFileDialog();
+            this.txtIDEvento = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(190, 11);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(125, 22);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Procurar Imagem";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // txtIDEvento
+            // 
+            this.txtIDEvento.Location = new System.Drawing.Point(99, 12);
+            this.txtIDEvento.Name = "txtIDEvento";
+            this.txtIDEvento.Size = new System.Drawing.Size(83, 20);
+            this.txtIDEvento.TabIndex = 1;
+            this.txtIDEvento.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID do Evento:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 52);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIDEvento);
+            this.Controls.Add(this.btnAbrir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Imagem > Base 64";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.SaveFileDialog svd;
+        private System.Windows.Forms.TextBox txtIDEvento;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ImageToBase64/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageToBase64
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            if (txtIDEvento.Text.Trim() != string.Empty)
+            {
+                for (int i = 0; i < txtIDEvento.Text.Length; i++)
+                {
+                    if (!Char.IsNumber(txtIDEvento.Text[i]))
+                    {
+                        MessageBox.Show("Digite apenas números.");
+                        txtIDEvento.Text = string.Empty;
+                        return;
+                    }
+                }
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string base64 = ImageToBase64(ofd.FileName);
+
+                    FileInfo f = new FileInfo(ofd.FileName);
+
+                    Save(Application.StartupPath + "\\" + "INSERT_" + f.Name.Replace(f.Extension, string.Empty) + ".SQL",
+                          "UPDATE eventos SET imagem64 = '" + base64 + "' WHERE idEvento = " + txtIDEvento.Text);
+
+                    txtIDEvento.Text = string.Empty;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Informe o ID do evento.");
+            }
+        }
+
+        private bool Save(string strPath, string base64)
+        {
+            try
+            {
+                FileStream fs = new FileStream(strPath, FileMode.Create, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(base64);
+                sw.Close();
+                fs.Close();
+                
+                Process.Start(Application.StartupPath);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private string ImageToBase64(string strPath)
+        {
+            using (Image image = Image.FromFile(strPath))
+            {
+                using (MemoryStream m = new MemoryStream())
+                {
+                    image.Save(m, image.RawFormat);
+                    byte[] imageBytes = m.ToArray();
+
+                    // Convert byte[] to Base64 String
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
+            }
+        }
+    }
+}
